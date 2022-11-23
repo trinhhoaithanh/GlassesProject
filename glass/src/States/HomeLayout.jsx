79b7d2f6cc8563d1be4aee45a8cf9865e966e6bc
ththanh <hoaithanhtrinh2002@gmail.com>
2022-11-23 18:56:37 +0700

    layout

---
+++ glass/src/States/HomeLayout.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class HomeLayout extends Component {
+  render() {
+    return (
+      <div className='glass'>
+       
+        <div className='overlay'>
+        
+        </div>
+        <div className='overlay-header'>
+        <h className='title'>TRY GLASSES APP ONLINE</h>
+        </div>
+        <div className='row model'>
+            <div className='col-6'>
+               
+            </div>
+            <div className='col-5'>
+               
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
